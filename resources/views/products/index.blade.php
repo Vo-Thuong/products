c261ca897ccc7d518c6847720b71e89871c3ba14
vothuong0610 <hoangrink2@gmail.com>
2025-08-27 19:05:03 +0700

    Cập nhật code: sửa chức năng ...

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,13 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Danh sách sản phẩm</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> -->
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 </head>
 
 <body>
     <div class="container">
-        <h1>Sản phẩm</h1>
-        <a href="{{ route('products.create') }}" class="btn">+ Thêm sản phẩm</a>
+        <h1>Book đi nào. Mãi bên nhau bạn nhé!</h1>
+        <p class="subtitle">Top nơi ở sang chảnh đón hè đã sẵn sàng trên Travelcon. Book đi ngay nè.</p>
+
+        <a href="{{ route('products.create') }}" class="btn add-btn">+ Thêm sản phẩm</a>
 
         <div class="grid">
             @foreach ($products as $product)
@@ -18,12 +21,23 @@
                 @if ($product['image'])
                 <img src="{{ asset('storage/' . $product['image']) }}" alt="Ảnh sản phẩm">
                 @endif
-                <h2>{{ $product['name'] }}</h2>
-                <p>{{ $product['description'] }}</p>
-                <p class="price">{{ number_format($product['price'], 0, ',', '.') }} VND</p>
-                <div class="actions">
-                    <a href="{{ route('products.edit', $product['id']) }}" class="btn">Sửa</a>
-                    <a href="{{ route('products.destroy', $product['id']) }}" class="btn btn-delete">Xóa</a>
+
+                <div class="info">
+                    <div class="rating">★★★★★ <span>{{ $product['reviews'] ?? 0 }} Review</span></div>
+                    <h2>{{ $product['name'] }}</h2>
+                    <p>{{ $product['description'] }}</p>
+                    <p class="price">Giá tiền <span>{{ number_format($product['price'], 0, ',', '.') }} đ</span></p>
+
+                    <div class="status">
+                        <span>Còn {{ $product['rooms_left'] ?? 0 }} phòng</span>
+                        <span>{{ $product['booked'] ?? 0 }} đã đặt</span>
+                    </div>
+
+                    <div class="actions">
+                        <a href="{{ route('products.edit', $product['id']) }}" class="btn">Sửa</a>
+                        <a href="{{ route('products.destroy', $product['id']) }}" class="btn btn-delete">Xóa</a>
+                        <a href="#" class="btn btn-order">Đặt Ngay ⚡</a>
+                    </div>
                 </div>
             </div>
             @endforeach
